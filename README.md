a520ab87abb832cc2faae0b408abc78e6ad389aa
Md. Ashiqur Rahman <ashiqurr04@gmail.com>
2023-04-14 13:45:37 +0600

    changes added necessary files
    update readme file

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # vuejs-nodejs-example
+
 Example Project to demonstrate how to use Vue.js with nodejs backend
+
+# Run Project Command
+
+// nodejs API (Terminal 1)
+cd api (change it to API directory)
+npm run dev
+// Vue app (Terminal 2)
+cd my-app (change it to app directory)
+npm run serve
--- my-app/src/components/Dashboard.vue
+++ my-app/src/components/Dashboard.vue
@@ -2,17 +2,17 @@
   <div class="hello">
     <Header />
     <div class="container mrgnbtm">
-          <div class="row">
-            <div class="col-md-8">
-                <CreateUser @createUser="userCreate($event)" />
-            </div>
-            <div class="col-md-4">
-                <DisplayBoard :numberOfUsers="numberOfUsers" @getAllUsers="getAllUsers()" />
-            </div>
-          </div>
+      <div class="row">
+        <div class="col-md-8">
+          <CreateUser @createUser="userCreate($event)" />
+        </div>
+        <div class="col-md-4">
+          <DisplayBoard :numberOfUsers="numberOfUsers" @getAllUsers="getAllUsers()" />
+        </div>
+      </div>
     </div>
     <div class="row mrgnbtm">
-        <Users v-if="users.length > 0" :users="users" />
+      <Users v-if="users.length > 0" :users="users" />
     </div>
   </div>
 </template>
@@ -33,10 +33,10 @@ export default {
     Users
   },
   data() {
-      return {
-          users: [],
-          numberOfUsers: 0
-      }
+    return {
+      users: [],
+      numberOfUsers: 0
+    }
   },
   methods: {
     getAllUsers() {
@@ -54,7 +54,7 @@ export default {
       });
     }
   },
-  mounted () {
+  mounted() {
     this.getAllUsers();
   }
 }
